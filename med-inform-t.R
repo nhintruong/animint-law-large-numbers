4fab5bb8e3e70027661e68300af55ecfa29beda6
ampurr <99299900+ampurr@users.noreply.github.com>
2023-04-24 02:57:46 -0400

    sampling is finally wking right :>

--- med-inform-t.R
+++ med-inform-t.R
@@ -1,4 +1,5 @@
-d6 <- sample(x = 1:6, size = 10, replace = TRUE)
+set.seed(135)
+d6 <- sample(x = 1:6, size = 1000, replace = TRUE)
 
 data_vector <- d6
 
@@ -6,20 +7,16 @@ population_mean <- data_vector |> mean()
 
 # empty vector
 sample_means <- vector(mode = "numeric",
-                       length = 10)
+                       length = 1000)
 
 # random sampling
 for (i in seq_along(data_vector)) {
-  sample_means[i] <- data_vector |>
-    sample(replace = TRUE)
+  sample_means[i] <- data_vector |> 
+    sample(size = i, replace = TRUE) |> 
+    mean()
 }
+
+plot(sample_means)
 
 # generate tibble
 sample_tbl <- sample_means |> tibble::as_tibble()
-
-# from ani.lln
-for (i in 1:n) {
-  d = colMeans(matrix(replicate(np, FUN(i, mu)), i))
-  m = c(m, d)
-  x = rbind(x, range(d))
-}
