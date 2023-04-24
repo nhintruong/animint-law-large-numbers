4d458c49f31d01e6f0caa220c90329645b9d2bca
ampurr <99299900+ampurr@users.noreply.github.com>
2023-04-24 18:54:08 -0400

    finished w/ static animation

--- med-inform-t.R
+++ med-inform-t.R
@@ -1,5 +1,7 @@
-set.seed(135)
-d6 <- sample(x = 1:6, size = 1000, replace = TRUE)
+library(animint2)
+
+set.seed(120)
+d6 <- sample(x = 1:6, size = 100, replace = TRUE)
 
 data_vector <- d6
 
@@ -7,7 +9,7 @@ population_mean <- data_vector |> mean()
 
 # empty vector
 sample_means <- vector(mode = "numeric",
-                       length = 1000)
+                       length = 100)
 
 # random sampling
 for (i in seq_along(data_vector)) {
@@ -16,7 +18,24 @@ for (i in seq_along(data_vector)) {
     mean()
 }
 
-plot(sample_means)
+# generate dataframe
+sample_df <- sample_means |> 
+  data.frame() |>
+  tibble::rowid_to_column(var = "n")
 
-# generate tibble
-sample_tbl <- sample_means |> tibble::as_tibble()
+# generate list of dataframes
+list_dfs
+
+# plot construction
+static_lln_plot <- sample_tbl |> 
+  ggplot() +
+  aes(x = n, y = sample_df$sample_means) + 
+  labs(y = "sample mean") +
+  geom_point(showSelected = "n", alpha = 0.5) +
+  geom_hline(yintercept = population_mean, colour = "red")
+static_lln_plot
+
+# plot animation
+ani_lln_plot <- animint(static_lln_plot)
+ani_lln_plot$time <- list(variable = "n", ms = 200)
+ani_lln_plot
